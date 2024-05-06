d4800754ea39f07d08b6e37dc9dd9156eac8f320
LAQDQ <lersahkyo@gmail.com>
2024-05-07 02:36:39 +0900

    change serializer init file

--- yuruboo/backend/backend/users/serializers/__init__.py
+++ yuruboo/backend/backend/users/serializers/__init__.py
@@ -1,5 +1,8 @@
 from .genre import GenreSerializer
 from .gathering import GatheringSerializer
+from .message import MessageSerializer
+from .ownership import OwnershipSerializer
+from .participation import ParticipationSerializer
 
 __all__ = [
     "GenreSerializer",
