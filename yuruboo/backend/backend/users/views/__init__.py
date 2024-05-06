ad4a282c17fcfb99b5ff4fa0b798b1bd12969ef8
LAQDQ <lersahkyo@gmail.com>
2024-05-07 02:44:13 +0900

    change viewset init file

--- yuruboo/backend/backend/users/views/__init__.py
+++ yuruboo/backend/backend/users/views/__init__.py
@@ -1,7 +1,13 @@
 from .genre import GenreViewSet
 from .gathering import GatheringViewSet
+from .ownership import OwnershipViewSet
+from .message import MessageViewSet
+from .participation import ParticipationViewSet
 
 __all__ = [
     "GenreViewSet",
     "GatheringViewSet",
+    "OwnershipViewSet",
+    "MessageViewSet",
+    "ParticipationViewSet",
 ]
