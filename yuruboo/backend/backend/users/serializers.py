0f4538d81c89fb363d2b914d0f5adbc73fb6a6ad
LAQDQ <lersahkyo@gmail.com>
2024-05-11 19:47:15 +0900

    create serializer file in user

---
+++ yuruboo/backend/backend/users/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.Serializer):
+    class Meta:
+        model = User
+        fields = ('user_name', 'email', 'id')
