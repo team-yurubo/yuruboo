d2f569115d8bc9c78ab28fbe8131ce94929dab3a
LAQDQ <lersahkyo@gmail.com>
2024-05-13 03:25:19 +0900

    edit serializer field(color)

--- yuruboo/backend/backend/users/serializers.py
+++ yuruboo/backend/backend/users/serializers.py
@@ -5,4 +5,4 @@ from .models import CustomUser
 class CustomUserSerializer(serializers.Serializer):
     class Meta:
         model = CustomUser
-        fields = ('id','email', 'user_name', 'is_staff','is_active','profile')
+        fields = ('id','email', 'user_name', 'is_staff','is_active','profile','color')
