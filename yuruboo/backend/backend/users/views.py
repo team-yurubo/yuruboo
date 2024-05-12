2e5672d44d7c897ff95d9456e3f88674fd3a01e0
kento3485 <kento3485@gmail.com>
2024-05-12 13:29:39 +0900

    Modify serializers and views

--- yuruboo/backend/backend/users/views.py
+++ yuruboo/backend/backend/users/views.py
@@ -1,7 +1,9 @@
 from .models import CustomUser
 from .serializers import UserSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
+    permission_classes =[IsAuthenticated]
