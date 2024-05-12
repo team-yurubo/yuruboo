34635eba5b79e69d57d4bedea6b71c21838d6c68
kento3485 <kento3485@gmail.com>
2024-05-12 13:47:05 +0900

    Change naming(User -> CustomUser)

--- yuruboo/backend/backend/users/views.py
+++ yuruboo/backend/backend/users/views.py
@@ -1,9 +1,9 @@
 from .models import CustomUser
-from .serializers import UserSerializer
+from .serializers import CustomUserSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-class UserViewSet(viewsets.ModelViewSet):
+class CustomUserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
-    serializer_class = UserSerializer
-    permission_classes =[IsAuthenticated]
+    serializer_class = CustomUserSerializer
+    permission_classes =[AllowAny]
