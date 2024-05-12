3a4d43c25332d3fd6fa1a416513c5d14d5462425
kento3485 <kento3485@gmail.com>
2024-05-12 20:17:28 +0900

    Change serializers(gathering, participation)

--- yuruboo/backend/backend/app/models/genre.py
+++ yuruboo/backend/backend/app/models/genre.py
@@ -4,4 +4,4 @@ import uuid
 class Genre(models.Model):
     id : models.UUIDField = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name : models.CharField = models.CharField(max_length=255, help_text="ジャンル名")
-    icon : models.URLField = models.URLField(help_text="アイコンURL")
+    icon : models.URLField = models.URLField(help_text="アイコンURL", max_length=1023)
