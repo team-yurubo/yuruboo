3f1eaa656b5a4cfd00325290ba1080fe3973d115
kento3485 <kento3485@gmail.com>
2024-05-12 20:28:24 +0900

    Change permission class

--- yuruboo/backend/backend/app/views/genre.py
+++ yuruboo/backend/backend/app/views/genre.py
@@ -1,11 +1,8 @@
 from ..models import Genre
 from ..serializers import GenreSerializer
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    permission_classes = [IsAuthenticated]
-
     
