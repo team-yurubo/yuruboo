efad1aefee4b9bd12f19103032eb6927959ec5c4
kento3485 <kento3485@gmail.com>
2024-05-16 19:10:25 +0900

    Change message endpoint

--- yuruboo/backend/backend/app/serializers/message.py
+++ yuruboo/backend/backend/app/serializers/message.py
@@ -3,13 +3,18 @@ from ..models import Message, Gathering
 from .user import CustomUserSerializer
 
 class MessageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Message
+        fields = ["id", "gathering", "body", "sender", "created_at"]
+
+class MessageDetailSerializer(serializers.ModelSerializer):
     sender = CustomUserSerializer()
     class Meta:
         model = Message
         fields = ["id", "gathering", "body", "sender", "created_at"]
 
 class MessageLogsSerializer(serializers.ModelSerializer):
-    messages = MessageSerializer(many=True)
+    messages = MessageDetailSerializer(many=True)
     
     class Meta:
         model = Gathering
