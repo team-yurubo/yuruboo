2f6b6078bf151a147196fa4bc92581aea23cbb5f
kento3485 <kento3485@gmail.com>
2024-05-16 15:46:08 +0900

    Change to use detail url

--- yuruboo/frontend/src/features/components/chat/api.tsx
+++ yuruboo/frontend/src/features/components/chat/api.tsx
@@ -14,23 +14,13 @@ export const getGatheringId = async (): Promise<string> => {
 };
 
 export const getMessages = async (gathering_id: string) => {
-    const response = await fetch(`http://localhost:8000/messagelogs/`, {
+    const response = await fetch(`http://localhost:8000/messagelogs/${gathering_id}/`, {
       credentials: 'include', // クッキーを含めるためのオプション
     });
     const data = await response.json();
 
     console.log(data);
 
-    let return_data = [];
-
-    console.log("start for loop");
-    // gathering_id に対応する MessageLog を返す
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id === gathering_id) {
-        return_data = (data[i].messages);
-      }
-    }
-    console.log(return_data);
-    return return_data;
+    return data.messages;
 };
 
